c1989b32b9e2ed9998dc5044593350f9c10cd2c8
findforhad <forhadhossenbabu@gmail.com>
2019-11-17 06:15:03 +0600

    letest update

--- src/components/HLD/hld.component.jsx
+++ src/components/HLD/hld.component.jsx
@@ -7,83 +7,85 @@ import { Link } from "react-router-dom";
 import "./hld.styles.scss";
 
 const HLD = () => (
-  <Container className="hld__wrapper">
-    <Row>
-      <Col md={6}>
-        <Fade>
-          <h4>Helping Learner Drivers</h4>
-          <div className="line"></div>
-          <p>
-            Finding an instructor can be a tricky business. With the best in the
-            business seemingly forever booked up, and often behind the wheel and
-            unable to take your call, you can feel like you’re getting passed
-            around and around. We’re glad to say that this is a thing of the
-            past, and you can now find your perfect instructor with the click of
-            a button:
-          </p>
-          <TextCard
-            span="Instant Access"
-            para="to available instructors in your local area"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
-          />
-          <TextCard
-            span="Secure Payments"
-            para="made online so you never have to worry about withdrawing cash"
-            sign={require("./assets/sign.png")}
-          />
-          <TextCard
-            span="Multiple Bookings:"
-            para="perfect for when your test is getting closer"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
-          />
+  <Container>
+    <div className="hld__wrapper">
+      <Row>
+        <Col md={6}>
+          <Fade>
+            <h4>Helping Learner Drivers</h4>
+            <div className="line"></div>
+            <p>
+              Finding an instructor can be a tricky business. With the best in
+              the business seemingly forever booked up, and often behind the
+              wheel and unable to take your call, you can feel like you’re
+              getting passed around and around. We’re glad to say that this is a
+              thing of the past, and you can now find your perfect instructor
+              with the click of a button:
+            </p>
+            <TextCard
+              span="Instant Access"
+              para="to available instructors in your local area"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
+            <TextCard
+              span="Secure Payments"
+              para="made online so you never have to worry about withdrawing cash"
+              sign={require("./assets/sign.png")}
+            />
+            <TextCard
+              span="Multiple Bookings:"
+              para="perfect for when your test is getting closer"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
 
-          <TextCard
-            span="Speak Directly"
-            para="to your instructor when their current lesson has finished"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
+            <TextCard
+              span="Speak Directly"
+              para="to your instructor when their current lesson has finished"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
+            <TextCard
+              span="Proven Star Rating System"
+              para="allows you to find the very best of the best"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
+            <TextCard
+              span="Pass Faster"
+              para="than with any other driving platform"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
+            <TextCard
+              span="Search Offers"
+              para="such as bulk discounts and introductory sessions to find the deal for you"
+              sign={require("./assets/sign.png")}
+              imgHeight="16px"
+            />
+            <p>
+              With everything you need available in an instant in the palm of
+              your hand, you’ll have more time and energy than ever before to
+              give every lesson your best and pass first time!{" "}
+            </p>
+            <div className="button">
+              <Link to="/">Pass Faster, Pass First Time</Link>
+            </div>
+          </Fade>
+        </Col>
+        <Col md={6} className="hld__image__sec">
+          <img
+            alt="hldGirl"
+            src={require("./assets/girl.png")}
+            className="img-fluid"
           />
-          <TextCard
-            span="Proven Star Rating System"
-            para="allows you to find the very best of the best"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
-          />
-          <TextCard
-            span="Pass Faster"
-            para="than with any other driving platform"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
-          />
-          <TextCard
-            span="Search Offers"
-            para="such as bulk discounts and introductory sessions to find the deal for you"
-            sign={require("./assets/sign.png")}
-            imgHeight="16px"
-          />
-          <p>
-            With everything you need available in an instant in the palm of your
-            hand, you’ll have more time and energy than ever before to give
-            every lesson your best and pass first time!{" "}
-          </p>
-          <div className="button">
-            <Link to="/">Pass Faster, Pass First Time</Link>
+          <div className="girl__bottom">
+            <p>Know your instructor before taking a lesson ! </p>
           </div>
-        </Fade>
-      </Col>
-      <Col md={6} className="hld__image__sec">
-        <img
-          alt="hldGirl"
-          src={require("./assets/girl.png")}
-          className="img-fluid"
-        />
-        <div className="girl__bottom">
-          <p>Know your instructor before taking a lesson ! </p>
-        </div>
-      </Col>
-    </Row>
+        </Col>
+      </Row>
+    </div>
   </Container>
 );
 
--- src/components/removal-footer/removal-footer.component.jsx
+++ src/components/removal-footer/removal-footer.component.jsx
@@ -2,26 +2,41 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const RemovalFooter = () => (
-  <div
-    style={{
-      display: "flex",
-      justifyContent: "space-around",
-      padding: "20px 0",
-      background: "black"
-    }}
-  >
-    <Link to="/">Home</Link>
-    <Link to="/signui-in">SignIn</Link>
-    <Link to="/blog">Blog</Link>
-    <Link to="/learner-driver">Learner Driver</Link>
-    <Link to="/driving-instructor">Driving Instructor</Link>
-    <Link to="/how-it-works-learners">How It Works Learners</Link>
-    <Link to="/how-it-works-instructor">How It Works Instructor</Link>
-    <Link to="/check-out">Check Out</Link>
-    <Link to="/book-your-lession">Book Your Lession</Link>
-    <Link to="/privacy-policy">Privacy Policy</Link>
-    <Link to="/terms">Terms or use</Link>
-  </div>
+  <ul style={{ background: "black" }}>
+    <Link to="/" style={{ margin: "5px" }}>
+      Home
+    </Link>
+    <Link to="/signui-in" style={{ margin: "5px" }}>
+      SignIn
+    </Link>
+    <Link to="/blog" style={{ margin: "5px" }}>
+      Blog
+    </Link>
+    <Link to="/learner-driver" style={{ margin: "5px" }}>
+      Learner Driver
+    </Link>
+    <Link to="/driving-instructor" style={{ margin: "5px" }}>
+      Driving Instructor
+    </Link>
+    <Link to="/how-it-works-learners" style={{ margin: "5px" }}>
+      How It Works Learners
+    </Link>
+    <Link to="/how-it-works-instructor" style={{ margin: "5px" }}>
+      How It Works Instructor
+    </Link>
+    <Link to="/check-out" style={{ margin: "5px" }}>
+      Check Out
+    </Link>
+    <Link to="/book-your-lession" style={{ margin: "5px" }}>
+      Book Your Lession
+    </Link>
+    <Link to="/privacy-policy" style={{ margin: "5px" }}>
+      Privacy Policy
+    </Link>
+    <Link to="/terms" style={{ margin: "5px" }}>
+      Terms or use
+    </Link>
+  </ul>
 );
 
 export default RemovalFooter;
